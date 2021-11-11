202052803948fd9eb8dfa6f122e39027c5638468
timibadass <omoyenitimi@gmail.com>
2021-11-12 00:21:12 +0100

    add search component

---
+++ components/Search.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="search">
+    <form class="search__form" @input="emitUserInput" @submit.prevent>
+      <input
+        id="search"
+        v-model="title"
+        class="search__input-field"
+        type="search"
+        name="search"
+        placeholder="Enter movie name"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      title: '',
+    }
+  },
+  methods: {
+    emitUserInput() {
+      const currentValue = this.title
+      setTimeout(() => {
+        if (currentValue === this.title) {
+          this.$emit('search-title', this.title)
+        }
+      }, 800)
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.search {
+  width: 100%;
+  max-width: 350px;
+  margin: 30px auto;
+  &__form {
+    width: 100%;
+  }
+  &__input-field {
+    height: 40px;
+    padding: 20px;
+    box-sizing: border-box;
+    width: 100%;
+    border-radius: 4px;
+    font-family: 'Source Sans Pro', sans-serif;
+    text-transform: capitalize;
+    &::placeholder {
+      font-weight: 300;
+    }
+    border: 1px solid #495057;
+  }
+}
+</style>
